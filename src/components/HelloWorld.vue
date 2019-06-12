ce494b3ae759c5d747043da3a61c5b3108f392f0
HF <fumi226045@gmail.com>
2019-06-12 15:35:51 +0900

    資料分

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,22 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <counter />
+    <ItemList />
   </div>
 </template>
 
 <script>
 import Counter from "./Counter.vue";
+import ItemList from "./ItemList.vue";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
   },
   components: {
-    Counter
+    Counter,
+    ItemList
   }
 };
 </script>
